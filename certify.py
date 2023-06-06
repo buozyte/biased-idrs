Fixes: computation of sigma for id models

certify.py
@@ -53,7 +53,6 @@ parser.add_argument('--num_nearest', default=20, type=int,
                     help='How many nearest neighbors to use')
 args = parser.parse_args()
 
-# python certify.py cifar10 trained_models/cifar10/resnet110/noise_0.50/checkpoint.pth.tar 0.50 data/certify/cifar10/resnet110/noise_0.50/test --skip 100 --batch 400 --input_dependent True
 
 if __name__ == "__main__":
 
@@ -93,7 +92,7 @@ if __name__ == "__main__":
         # obtain knn distances of test data
         dist_computer = KNNDistComp(train_dataset, args.num_workers, device)
         test_dataloader = DataLoader(test_dataset, batch_size=100, shuffle=False,
-                                    num_workers=0, pin_memory=False)
+                                     num_workers=0, pin_memory=False)
         distances = torch.zeros(10000)
         for i, (test_data, labels) in enumerate(test_dataloader):
             distances[i * 100:(i + 1) * 100] = dist_computer.compute_dist(test_data, 20, args.norm)
@@ -101,7 +100,7 @@ if __name__ == "__main__":
     
         smoothed_classifier = InputDependentRSClassifier(base_classifier=base_classifier, num_classes=num_classes,
                                                          sigma=args.base_sigma, distances=distances, rate=args.rate,
-                                                         k=args.num_nearest, m=norm_const, device=device).to(device)
+                                                         m=norm_const, device=device).to(device)
     else:
         smoothed_classifier = RandSmoothedClassifier(base_classifier=base_classifier, num_classes=num_classes,
                                                      sigma=args.base_sigma, device=device).to(device)

models/input_dependent_rs.py
@@ -15,7 +15,7 @@ class InputDependentRSClassifier(nn.Module):
     Function defining the variance sigma based on paper: intriguing properties of input-dependent RS.
     """
 
-    def __init__(self, base_classifier, num_classes, sigma, distances, rate, k, m, device, abstain=-1):
+    def __init__(self, base_classifier, num_classes, sigma, distances, rate, m, device, abstain=-1):
         """
         Initialize the randomly smoothed classifier
 
@@ -25,7 +25,6 @@ class InputDependentRSClassifier(nn.Module):
         :param distances:
         :param rate: semi-elasticity constant for chosen sigma function
         :param m: normalization constant for data set
-        :param k: number of nearest neighbours to consider
         :param device:
         :param abstain: value to be returned when smoothed classifier should abstain
         """
@@ -38,7 +37,6 @@ class InputDependentRSClassifier(nn.Module):
         self.distances = distances
         self.rate = rate
         self.m = m
-        self.k = k
         self.device = device
         self.abstain = abstain
 
@@ -51,7 +49,7 @@ class InputDependentRSClassifier(nn.Module):
         :return: variance w.r.t. current input
         """
 
-        return self.sigma * np.exp(self.rate * (1/self.k) * self.distances[x_index] - self.m)
+        return self.sigma * np.exp(self.rate * (self.distances[x_index] - self.m))
 
     def sample_under_noise(self, x, x_index, n, batch_size):
         """