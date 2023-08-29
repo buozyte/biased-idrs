f3f20add2b27850ecfc70b440f229c3cbc84e530
Donata Buozyte <donata.buozyte@tum.de>
2023-08-29 20:49:48 +0200

    Add max index for toy

--- scripts/test_knn_bias_func.sh
+++ scripts/test_knn_bias_func.sh
@@ -9,7 +9,7 @@ conda activate idrs
 
 python train.py toy_dataset_linear_sep linear_model trained_models/toy/noise_0.25/bias_0.0 --num_workers 2 --batch 250 --base_sigma 0.25 --epochs 10 --biased True --bias_func mu_knn_based --bias_weight 0
 
-python certify.py toy_dataset_linear_sep trained_models/toy/noise_0.25/bias_0.0/mu_knn_based/checkpoint_biased_id.pth.tar 0.25 data/certify/toy/noise_0.25/bias_0.0 --skip 1 --batch 200
+python certify.py toy_dataset_linear_sep trained_models/toy/noise_0.25/bias_0.0/mu_knn_based/checkpoint_biased_id.pth.tar 0.25 data/certify/toy/noise_0.25/bias_0.0 --skip 1 --batch 200 --index_max 90
 python certify.py toy_dataset_linear_sep trained_models/toy/noise_0.25/bias_0.0/mu_knn_based/checkpoint_biased_id.pth.tar 0.25 data/certify/toy/noise_0.25/bias_1.0 --skip 1 --batch 200 --biased True --bias_func mu_knn_based --bias_weight 1 --index_max 90
 
 # ---
