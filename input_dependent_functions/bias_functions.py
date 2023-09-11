Fix gradient base bias + integrate in seml script
TODO: how to pass trained (alternative) model to model definition

input_dependent_functions/bias_functions.py
@@ -1,16 +1,15 @@
 import torch
 import numpy as np
 
-BIAS_FUNCTIONS = [None, "mu_toy", "mu_knn_based"]
+BIAS_FUNCTIONS = [None, "mu_toy", "mu_knn_based", "mu_gradient_based"]
 
 
-def mu_toy(oracles, bias_weight, x_index, base_classifier, device):
+def mu_toy(oracles, x_index, base_classifier, device):
     """
     Input-dependent function to compute a bias for the toy example using a linear classifier.
     Note that this bias function is not Lipschitz continuous.
     
     :param oracles: output oracle for each sample based on the k nearest neighbours
-    :param bias_weight: "weight" of the bias
     :param x_index: index of the current point
     :param base_classifier: the base classifier
     :param device: pytorch device handling
@@ -24,7 +23,7 @@ def mu_toy(oracles, bias_weight, x_index, base_classifier, device):
     w = (weight[1, 0] - weight[0, 0]) / (weight[0, 1] - weight[1, 1])
     orthogonal_vector = torch.tensor([-w, 1]).to(device)
 
-    return bias_weight * orthogonal_vector * oracles[x_index]
+    return orthogonal_vector * oracles[x_index]
 
 
 def mu_toy_train(labels, orthogonal_vector, device, input_dim=2):
@@ -46,7 +45,7 @@ def mu_toy_train(labels, orthogonal_vector, device, input_dim=2):
     return bias * orthogonal_vector
 
 
-def mu_nearest_neighbour(x, x_index, nearest_neighbours, distances, device, bias_weight=1):
+def mu_nearest_neighbour(x, x_index, nearest_neighbours, distances, device):
     """
     Input-dependent function to compute a bias based on the direction from the current sample to it's nearest neighbour.
 
@@ -55,17 +54,16 @@ def mu_nearest_neighbour(x, x_index, nearest_neighbours, distances, device, bias
     :param nearest_neighbours: the k-nearest neighbours of x
     :param distances: the according distance to the k-nearest neighbours
     :param device: pytorch device handling
-    :param bias_weight: "weight" of the bias
     :return: bias w.r.t. current input
     """
 
     x_distances = distances[x_index]
     dist_weight = np.min([x_distances[0], np.sqrt(x_distances[0] * (x_distances[1] - x_distances[0]))])
 
-    return bias_weight * dist_weight * (nearest_neighbours[x_index][0].to(device) - x)
+    return dist_weight * (nearest_neighbours[x_index][0].to(device) - x)
 
 
-def mu_gradient(alt_classifier, x, device, bias_weight=1):
+def mu_gradient(alt_classifier, x, device):
     """
     Input-dependent function to compute a bias based using a trained alternative classifier h and it's gradient.
     Specifically, the directional vector is derived via the gradient of the logit difference
@@ -74,19 +72,27 @@ def mu_gradient(alt_classifier, x, device, bias_weight=1):
     :param alt_classifier: pre-trained alternative classifier for the given setting
     :param x: current sample
     :param device: pytorch device handling
-    :param bias_weight: "weight" of the bias
     :return: bias w.r.t. current input
     """
-
-    output = alt_classifier(x)
+    
+    # add "batch size" to sample -> required for evaluation of classifier with resnet
+    # TODO: check if also needed for other classifiers
+    rep_shape = [1]
+    for _ in range(len(x.shape)):
+        rep_shape.append(1)
+    x_ = x.repeat(rep_shape)
+    x_ = x_.to(device)
+    
+    output = torch.flatten(alt_classifier(x_))
+    
     _, (top1, top2) = torch.topk(output, 2)
     # compute the full jacobian of the alternative classifier at x
-    full_jacobian = torch.autograd.functional.jacobian(alt_classifier, x)
+    full_jacobian = torch.autograd.functional.jacobian(alt_classifier, x_)[0]
     # derivative of h_i w.r.t. input = i-th row of jacobian
-    grad_h_i = full_jacobian[top1]
-    grad_h_j = full_jacobian[top2]
+    grad_h_i = full_jacobian[top1][0]
+    grad_h_j = full_jacobian[top2][0]
 
     # TODO: define a strength
     strength = 1
 
-    return bias_weight * strength * (grad_h_i - grad_h_j)
+    return strength * (grad_h_i - grad_h_j)

models/biased_idrs.py
@@ -18,7 +18,7 @@ class BiasedIDRSClassifier(nn.Module):
     """
 
     def __init__(self, base_classifier, num_classes, sigma, device, bias_func=None, variance_func=None, oracles=None,
-                 bias_weight=1, lipschitz=0, knns=None, distances=None, rate=0, mean_distances=None, m=0, abstain=-1):
+                 bias_weight=1, lipschitz=0, knns=None, distances=None, rate=0, mean_distances=None, m=0, alt_classifier=None, abstain=-1):
         """
         Initialize the randomly smoothed classifier
 
@@ -34,6 +34,7 @@ class BiasedIDRSClassifier(nn.Module):
         :param rate: semi-elasticity constant for chosen sigma function
         :param mean_distances: mean of distances for every sample to its k-nearest neighbours
         :param m: normalization constant for data set
+        :param alt_classifier: pre-trained alternative classifier for the current task
         :param abstain: value to be returned when smoothed classifier should abstain
         """
 
@@ -56,6 +57,7 @@ class BiasedIDRSClassifier(nn.Module):
         self.rate = rate
         self.mean_distances = mean_distances
         self.m = m
+        self.alt_classifier = alt_classifier
         self.abstain = abstain
 
     def bias_id(self, x, x_index):
@@ -71,9 +73,11 @@ class BiasedIDRSClassifier(nn.Module):
             return torch.zeros_like(x)
         
         if self.bias_func == "mu_toy":
-            return bf.mu_toy(self.oracles, self.bias_weight, x_index, self.base_classifier, self.device)
+            return bf.mu_toy(self.oracles, x_index, self.base_classifier, self.device)
         if self.bias_func == "mu_knn_based":
             return bf.mu_nearest_neighbour(x, x_index, self.knns, self.distances, self.device)
+        if self.bias_func == "mu_gradient_based":
+            return bf.mu_gradient(self.alt_classifier, x, self.device)
 
         return torch.zeros_like(x)
 