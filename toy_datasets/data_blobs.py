d4cfa0f4751811d71896551cf4680f63609c27a9
Donata Buozyte <donata.buozyte@tum.de>
2023-09-25 21:23:24 +0200

    Remove plotting script -> working on new one

--- toy_datasets/data_blobs.py
+++ toy_datasets/data_blobs.py
@@ -8,56 +8,6 @@ import matplotlib.pyplot as plt
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
-def visualize_dataset_with_classifier_knn_based(data, labels, ls, model, bias_weight=1, knns=None, distances=None,
-                                                show=False, save=False, file_path="", add_file_name="", train=True):
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
-    data_1 = data[labels == 1]
-    data_2 = data[labels == 0]
-
-    if train:
-        plt.figure(2)
-    else:
-        plt.figure(3)
-    plt.scatter(data_1[:, 0], data_1[:, 1], label="class: 1", s=3, color='blue')
-    plt.scatter(data_2[:, 0], data_2[:, 1], label="class: 0", s=3, color='red')
-
-    if knns is not None and distances is not None:
-        for i in range(0, len(data)):
-            x_distances = distances[i]
-            dist_weight = np.min([x_distances[0], np.sqrt(x_distances[0] * (x_distances[1] - x_distances[0]))])
-
-            new_data = data[i, :] + bias_weight * dist_weight * (knns[i][0] - data[i, :])
-
-            plt.plot([data[i, 0], new_data[0]],
-                     [data[i, 1], new_data[1]], 'g-')
-
-    xx, yy = np.meshgrid(ls, ls)
-    inputs = np.c_[xx.ravel(), yy.ravel()]
-    outputs = model(torch.from_numpy(inputs).to(torch.float32).to(device))
-    _, pred = outputs.topk(1, 1, True, True)
-    pred = pred.t().cpu()
-    plt.contourf(xx, yy, pred.reshape(xx.shape), cmap=plt.cm.Spectral, alpha=0.1)
-    
-    plt.legend(fontsize=9)
-    plt.xlabel("x", fontsize=14)
-    plt.ylabel("y", fontsize=14)
-
-    # plt.xlim([min(ls)-0.2, max(ls)+0.2])
-    # plt.ylim([min(ls)-0.2, max(ls)+0.2])
-    if train:
-        plt.title("Train dataset", fontsize=16)
-    else:
-        plt.title("Test dataset", fontsize=16)
-    if show:
-        plt.show()
-    if save:
-        if train:
-            plt.savefig(f"{file_path}/visual_decision_boundary_train{add_file_name}.pdf")
-        else:
-            plt.savefig(f"{file_path}/visual_decision_boundary_test{add_file_name}.pdf")
-
-
 class ToyDatasetBlobsTrain(Dataset):
     """
     Clustered data samples.
